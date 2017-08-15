89430c69e71fd1e313b62b61dd4c0ee97c9014d5
SoengSaravit <soengsaravit@gmail.com>
2017-08-15 23:29:34 +0700

    Initial Commit

---
+++ UINavigationController-22-07-2017/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  UINavigationController-22-07-2017
+//
+//  Created by Soeng Saravit on 7/22/17.
+//  Copyright © 2017 Soeng Saravit. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "Screen 2"
+    }
+
+  
+    
+
+
+}
---
+++ UINavigationController-22-07-2017/ThirdViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdViewController.swift
+//  UINavigationController-22-07-2017
+//
+//  Created by Soeng Saravit on 7/22/17.
+//  Copyright © 2017 Soeng Saravit. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "Screen 3"
+    }
+}
